1fb373dc856e0d4700f6a41c6e83dd9b7fca039c
sujanmandana <sujanmandana@gmail.com>
2022-10-09 15:59:30 +0530

    commit

---
+++ file.js
@@ -0,0 +1,24 @@
+function twodArray(a, b) {
+    let arr = [];
+
+    // creating two dimensional array
+    for (let i = 0; i< a; i++) {
+        for(let j = 0; j< b; j++) {
+            arr[i] = [];
+        }
+    }
+
+    // inserting elements to array
+    for (let i = 0; i< a; i++) {
+        for(let j = 0; j< b; j++) {
+            arr[i][j] = j;
+        }
+    }
+    return arr;
+}
+
+const x = 2;
+const y = 3;
+
+const result = twodArray(x, y);
+document.getElementById("array").innerHTML = result;
